f963b62540edbb39bcc40c6c002defe3b4b0d1ab
Keita <Keystyle8686@gmail.com>
2016-12-04 20:03:04 +0900

    #3 アジアの為替レート

--- app/controllers/rate_controller.rb
+++ app/controllers/rate_controller.rb
@@ -1,8 +1,14 @@
 class RateController < ApplicationController
-  def show
-    uri = URI.parse('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDJPY%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
-    json = Net::HTTP.get(uri)
-    @result = JSON.parse(json)
-    p @result
+  def show_us
+    uri_usd = URI.parse('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDJPY%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
+    json_usd = Net::HTTP.get(uri_usd)
+    @result_usd = JSON.parse(json_usd)
+  end
+
+  def show_asia
+    uri_asia =
+    URI.parse('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22JPYCNY,JPYKRW,JPYIDR,JPYVND,JPYTHB,JPYMMK%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
+    json_asia = Net::HTTP.get(uri_asia)
+    @result_asia = JSON.parse(json_asia)
   end
 end
